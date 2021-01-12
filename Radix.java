7eca2182d34bec4fe1249ac03ea3a5dcc77a03fe
Timothy <>
2021-01-12 12:16:03 -0500

    added length method.

--- Radix.java
+++ Radix.java
@@ -4,4 +4,12 @@ public class Radix{
   public static int nth(int n, int col){
     return Math.abs(n/(int)Math.pow(10,col)%10);
   }
+
+  public static int length(int n){
+    if (n == 0){
+      return 1;
+    }
+    n = Math.abs(n);
+    return (int)Math.log10(n)+1;
+  }
 }
